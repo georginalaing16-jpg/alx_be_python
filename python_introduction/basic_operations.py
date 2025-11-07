5064f2a324f7824c9515996fd2591c5f0a0fa7e6
Georgina Laing <georginalaing16@gmail.com>
2025-11-07 16:36:00 +0000

    Add basic_operations.py to python_introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Assigning integers
+number1 = 10
+number2 = 5
+# Performing basic arithmetic operations
+# Addition
+sum_result = number1 + number2
+print("Addition of", number1, "and", number2, "is:", sum_result)
+# Subtraction
+difference_result = number1 - number2
+print("Subtraction of", number1, "and", number2, "is:", difference_result)
+# Multiplication
+product_result = number1 * number2
+print("Muiltiplication of", number1, "and", number2, "is", product_result)
