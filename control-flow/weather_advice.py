14a3802c4f7fe70c621ba0e6b96d3d0cf03f3128
Georgina Laing <georginalaing16@gmail.com>
2025-11-12 09:47:29 +0000

    Add weather_advice.py to control-flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,9 +1,9 @@
 # Prompt Use for Weather Information
-Weather_Input = input("What is the weather like today? (sunny, rainy, cold): ")
+Weather_Input = input("What's the weather like today? (sunny, rainy, cold): ")
 if Weather_Input == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif Weather_Input == "rainy":
-    print("Don't forget to take an umbrella.")
+    print("Don't forget your umbrella and a raincoat.")
 elif Weather_Input == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
