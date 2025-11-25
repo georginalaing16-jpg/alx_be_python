b200c07141d2115c2bf5f13b0848643bd2b464e5
Georgina Laing <georginalaing16@gmail.com>
2025-11-25 19:54:18 +0000

    Add bank_account.py to programming_paradigm

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -10,11 +10,11 @@ class BankAccount:
             return False
 
     def withdraw(self, amount):
-        if 0 < amount <= self.account_balance:
+        if amount <= self.account_balance:
             self.account_balance -= amount
             return True
         else:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
