6be1d7eadd25bcba4ac3060c66b958c914d14302
Georgina Laing <georginalaing16@gmail.com>
2025-11-25 22:46:29 +0000

    Add robust_division_calculator.py to programming_paradigm

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,11 +1,13 @@
-$ git pull origin main
-From https://github.com/georginalaing16-jpg/alx_be_python
- * branch            main       -> FETCH_HEAD
-Updating 158feaa..3cc3b2b
-error: Unable to create 'C:/Users/hp/alx_be_python/.git/index.lock': File exists.
-
-Another git process seems to be running in this repository, e.g.
-an editor opened by 'git commit'. Please make sure all processes
-are terminated then try again. If it still fails, a git process
-may have crashed in this repository earlier:
-remove the file manually to continue.
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(numerator)
+        denominator = float(denominator)
+    except ValueError:
+        return "Error: Please enter numeric values only."
+    
+    try:
+        result = numerator / denominator
+        print(f"The result of the division is: {result}")
+        return result
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
