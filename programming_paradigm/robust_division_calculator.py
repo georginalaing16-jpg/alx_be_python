ce9ed0b10168daf47d7f9a5f00da01bce1c2fd3e
Georgina Laing <georginalaing16@gmail.com>
2025-11-25 22:41:10 +0000

    Add robust_division_calculator.py to programming_paradigm

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+$ git pull origin main
+From https://github.com/georginalaing16-jpg/alx_be_python
+ * branch            main       -> FETCH_HEAD
+Updating 158feaa..3cc3b2b
+error: Unable to create 'C:/Users/hp/alx_be_python/.git/index.lock': File exists.
+
+Another git process seems to be running in this repository, e.g.
+an editor opened by 'git commit'. Please make sure all processes
+are terminated then try again. If it still fails, a git process
+may have crashed in this repository earlier:
+remove the file manually to continue.
